1bf0951c378ff4c2241b04a2451cacda7c8ead57
gregnani <fgregnani@gmail.com>
2023-05-14 12:54:16 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿﻿Console.WriteLine("Fluxograma da Engenharia");
+
+Console.Write("está se movendo (s/n)? ");
+bool estaSeMovendo = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+Console.Write("Deveria? ");
+bool deveria = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+string mensagem;
+
+if (!estaSeMovendo)
+{
+    if (!deveria)
+    {
+        mensagem = "perfeito!";
+    }
+    else
+    {
+        mensagem = "Use WD.";
+    }
+}
+else
+{
+    if (deveria)
+    {
+        mensagem = "perfeito!";
+    }
+    else
+    {
+        mensagem = "Use Silver Tape.";
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine(mensagem);
